65b80d171a523a51d477f68af24e3f9687e83743
olivetti <olivetti83@protonmail.com>
2022-05-29 16:13:39 +0200

    Añado main.tf

---
+++ infraestructura/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.16.0"
+        }
+    }
+    required_version = ">= 0.13"
+    backend "s3" {
+        bucket = "practica-final-cicd"
+        region = "eu-west-1"
+        encrypt = true
+    }
+    
+}
+#AWS Provider
+provider "aws" {
+    region = "eu-west-1"
+
+}
+#Bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = "practica-final-cicd"
+  acl    = "private"
+
+  tags {
+    Name        = "practica-final-cicd"
+    Environment = "Dev"
+  }
+}
